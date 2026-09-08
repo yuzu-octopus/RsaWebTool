8c2d5eabe22901b5d343bcc2206b8eaab60d7c3d
Yuzu Octopus <octopusyuzu@gmail.com>
2026-09-08 22:21:44 +0800

    feat(ui): adopt Astryx Dracula brand kit (astryx-dracula 0.1.2)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@astryxdesign/core/reset.css';
+import '@astryxdesign/core/astryx.css';
+import 'astryx-dracula/tokens.css';
+import 'astryx-dracula/theme.css';
+import { Theme } from '@astryxdesign/core/theme';
+import { astryxDraculaTheme } from 'astryx-dracula';
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Theme theme={astryxDraculaTheme} mode="dark">
+      <App />
+    </Theme>
   </StrictMode>,
 )
